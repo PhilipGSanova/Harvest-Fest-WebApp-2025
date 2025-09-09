ba28c01cd23a50ba8edc04cc74003aafb7f18549
PhilipGSanova <sanov2004@gmail.com>
2025-09-09 12:20:17 +0530

    Added supabase api to environment variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,8 @@
 import {createClient} from '@supabase/supabase-js';
 
-const supabaseUrl='https://ingujuageyaowinmqmgf.supabase.co';
-const supabaseKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZ3VqdWFnZXlhb3dpbm1xbWdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3NjczOTcsImV4cCI6MjA2OTM0MzM5N30.pmJgU9nidKph4M4kCqf3Ed4pirUU4r09R7FnkPGtvsU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 const supabase=createClient(supabaseUrl,supabaseKey);
 
 export default supabase;
